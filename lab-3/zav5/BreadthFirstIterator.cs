e0289bc127f79b4c3d634268ee84e9825a8206c3
Верніцький Роман Олегович <ipz233_vro@student.ztu.edu.ua>
2025-06-06 03:49:52 +0300

    second pull

---
+++ lab-3/zav5/BreadthFirstIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zav5
+{
+    public class BreadthFirstIterator : ILightNodeIterator
+    {
+        private readonly LightNode _root;
+
+        public BreadthFirstIterator(LightNode root)
+        {
+            _root = root;
+        }
+
+        public IEnumerable<LightNode> Traverse()
+        {
+            var queue = new Queue<LightNode>();
+            queue.Enqueue(_root);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                yield return current;
+
+                if (current is LightElementNode elementNode)
+                {
+                    foreach (var child in elementNode.Children)
+                    {
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ lab-3/zav5/DepthFirstIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zav5
+{
+    public class DepthFirstIterator : ILightNodeIterator
+    {
+        private readonly LightNode _root;
+
+        public DepthFirstIterator(LightNode root)
+        {
+            _root = root;
+        }
+
+        public IEnumerable<LightNode> Traverse()
+        {
+            var stack = new Stack<LightNode>();
+            stack.Push(_root);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                yield return current;
+
+                if (current is LightElementNode elementNode)
+                {
+                    for (int i = elementNode.Children.Count - 1; i >= 0; i--)
+                    {
+                        stack.Push(elementNode.Children[i]);
+                    }
+                }
+            }
+        }
+    }
+
+}
